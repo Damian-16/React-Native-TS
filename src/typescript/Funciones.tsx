346da41423f6e8e8b8ea655168ccd9ba84fd4d5b
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-05-21 11:54:32 -0400

    se añadio las funciones

--- src/typescript/Funciones.tsx
+++ src/typescript/Funciones.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+const sumar = (a:number,b:number):number => {
+    return a + b; 
+}
+const restar=(a:number,b:number):number[]=>{
+    return [a,b]//esto devuelve la suma de los elementos del array x ej en este caso se ingresa 2 y 3 entonces da 23
+}
 export const Funciones = () => {
     return (
         <>
-            
+            <h3>Funciones</h3>
+           <span> El resultado es:  {sumar(10,1)},{restar(2,3)}</span>
         </>
     )
 }
