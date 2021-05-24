4a5fc301559a415635f9720f829bd5d8a4d6c0a0
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-05-24 15:36:24 -0400

    se mapeo la forma de la api y se trajo las imagenes y sus informaciones para la tabla

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -3,16 +3,35 @@ import { reqResApi } from '../api/reqRes'
 import { ReqResListado, Usuario } from '../interfaces/reqRes';
 
 export const Usuarios = () => {
-    const [usuario, setUsuario] = useState<Usuario[]>([])
+    const [usuarios, setUsuarios] = useState<Usuario[]>([])
     useEffect(() => {
      //llamado al API
     //  reqResApi.get('/users')
     reqResApi.get<ReqResListado>('/users')
      .then(resp=>{
          console.log(resp.data.data);
+         setUsuarios(resp.data.data)
      })
      .catch(console.log);
     }, [])
+
+
+    const renderItem = ({id,first_name,last_name,email,avatar}:Usuario )=>{
+        return(
+            <tr key={ id.toString() }>
+                <td>
+                    <img 
+                    src={avatar} 
+                    alt={first_name}
+                    style={{
+                        width: 35,
+                        borderRadius: 100
+                    }}/></td>
+                <td>{first_name} {last_name}</td>
+                <td>{email}</td>
+            </tr>
+        )
+    }
     return (
         <>
             <h3>Usuarios</h3>
@@ -24,6 +43,9 @@ export const Usuarios = () => {
                         <th>email</th>
                     </tr>
                 </thead>
+                <tbody>{
+                    usuarios.map(usuario=>renderItem(usuario))
+                    }</tbody>
             </table>
         </>
     )
