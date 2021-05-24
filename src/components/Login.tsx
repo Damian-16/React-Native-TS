d0fa9a2bcc551baaf4e79d05d18b83446f3a2fa5
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-05-24 03:12:30 -0400

    se añadio el reducer y su action y su dispatch

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,60 @@
-import {useReducer} from 'react'
+import {useReducer,useEffect} from 'react'
+interface AuthState{
+    validando:boolean;
+    token:string | null;
+    username:string;
+    nombre:string;
+
+}
+const initialState:AuthState ={
+    validando:true,
+    token:null, //no esta autenticado
+    username:'',
+    nombre:'',
+
+}
+type AuthAction ={type:'logout'};
+
+const authReducer = (state:AuthState,action:AuthAction):AuthState =>{
+// el state no deberia modificarse
+switch (action.type) {
+    case 'logout':
+        return{
+            validando:false,
+            token:null,
+            nombre:'',
+            username:'',
+        }
+        
+        break;
+
+    default:
+        return state;
+}
+}
+
 
 export const Login = () => {
-    // const [state, dispatch] = useReducer(reducer, initialState, init)
-    return (
-        <>
-        <h3>Login</h3>
+   const [{validando}, dispatch] = useReducer(authReducer, initialState,)
+   //en vez de state desestructuramos con {} para traer validando
+   useEffect(() => {
+       setTimeout( () => {
+           dispatch({type:'logout'})
+       },1500);
+   }, []);
+   if(validando){
+       return (
+           <>
+           <h3>Login</h3>
         <div className="alert alert-info">
             Validando...
         </div>
+           </>
+       )
+   }  
+   return (
+        <>
+       
         <div className="alert alert-danger">
             No autenticado
         </div>
