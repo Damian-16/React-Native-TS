19c113761b439392ba396d69f92bcf8969b3edc9
Damian Pinedo <DamianMorganti16@hotmail.com>
2021-05-25 23:41:52 -0400

    se agrego un custom hook del formulario para evitar el error de poneren un input algo mal escrito

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { useState } from 'react';
 
-export const useForm = () => {
+export const useForm = <T extends Object>(formulario:T) => {
+    const [state, setState] = useState(formulario);
   
+    // const[formulario,setFormulario]= useState({
+    //     email:'test@test.com',
+    //     password:'123456'
+    // })
+    const onChange =(value:string, campo: keyof T)=>{
+        setState({
+            ...state,
+            [campo]:value// al ponerle[]a campo lo que estamos haciendo es decirle a js que lo compute como el valor que recibe de parametros
+        })
 
-    
+    }
+    return {
+        ...state,
+        formulario:state,
+        onChange,
+            
+    }
 }
